bd773cea665dbfe307359a45842d73fd03707db0
Anton Kulpinov <a.kulpinov@gmail.com>
2025-02-18 18:33:15 +0400

    add skipdns flag & change default logging level

--- cmd/texporter/flags.go
+++ cmd/texporter/flags.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/spf13/pflag"
+import (
+	"github.com/spf13/pflag"
+)
 
 type Flags struct {
 	InterfaceName string
@@ -8,16 +10,18 @@ type Flags struct {
 	IPRangesFile  string
 	Port          string
 	BindAddress   string
+	SkipDNS       bool
 }
 
 func ParseFlags() *Flags {
 	flags := &Flags{}
 
 	pflag.StringVarP(&flags.InterfaceName, "interface", "i", "eth0", "Interface name to attach")
-	pflag.StringVarP(&flags.LogLevel, "log-level", "l", "info", "Log level")
+	pflag.StringVarP(&flags.LogLevel, "log-level", "l", "error", "Log level")
 	pflag.StringVarP(&flags.IPRangesFile, "ip-ranges-filename", "f", "", "IP Ranges filename")
 	pflag.StringVarP(&flags.BindAddress, "bind-address", "b", "", "Bind address")
 	pflag.StringVarP(&flags.Port, "port", "p", "2112", "Port to listen to")
+	pflag.BoolVarP(&flags.SkipDNS, "skip-dns", "n", false, "Skip reverse DNS lookups")
 
 	pflag.Parse()
 
